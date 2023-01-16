6ea66161e8a2facf69a37ca5e1f5af2d4f1d60a6
alicefmc <alicecosta101208@gmail.com>
2023-01-15 21:20:23 -0300

    Projeto_43

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Dica: Sempre mudando, não constante!!", 100,70)
+// adicione código para exibir o restante dos nomes e as dicas.
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N",700,160)
+    fill("lightblue")
+    text("Dica: Faz uma tarefa particular!!", 700,180)
+
+    fill("white")
+    textSize(15)
+    text("A T E D A S B A",100,250)
+    fill("lightblue")
+    text("Dica: Faz uma tarefa particular!!", 100,270)
+
+}
